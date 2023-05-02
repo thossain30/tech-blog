5e56b26d3fba7d68a9a4d6f5af906ba53c749676
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-05-01 20:14:22 -0400

    Worked on handlebar logic

--- main/public/js/profile.js
+++ main/public/js/profile.js
@@ -1,13 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#blog-name').value.trim();
+  const title = document.querySelector('#blog-title').value.trim();
   const description = document.querySelector('#blog-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (title && description) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, description }),
       headers: {
         'Content-Type': 'application/json',
       },
