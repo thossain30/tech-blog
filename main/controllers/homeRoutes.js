f74159316593975c04574ccb03ac260ca9014184
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-05-02 17:01:21 -0400

    added comments

--- main/controllers/homeRoutes.js
+++ main/controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Blog, User } = require('../models');
+const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
--- main/models/index.js
+++ main/models/index.js
@@ -1,13 +1,14 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
   
-  Blog.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
   
-  module.exports = { User, Blog };
+module.exports = { User, Blog, Comment };
