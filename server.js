eb4d79aadd3dab565af9c22c9092a4dcbada5c62
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-05-02 18:58:20 -0400

    redid file structure

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./main/controllers');
-const helpers = require('./main/utils/helpers');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-const sequelize = require('./main/config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -41,5 +43,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT || 3000, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
